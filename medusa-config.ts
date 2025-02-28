b000e2b472b1673d30c839e4615fde0cef880647
aman-Racktrendz <gaurit.sup@gmail.com>
2025-02-27 16:08:01 -0800

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -5,6 +5,8 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
    admin: {
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    backendUrl: process.env.MEDUSA_BACKEND_URL
+
   },
 
   projectConfig: {
